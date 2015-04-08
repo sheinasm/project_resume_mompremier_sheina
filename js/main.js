9f37a26799764225062e07f3f1aada6aadf58f75
Sheina Mompremier <sheina.sally@gmail.com>
2015-04-08 01:57:42 -0400

    add textillate plugin and style

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@ $( document ).ready(function() {
 
     $.backstretch("img/tumblr_mmdvbbvvMx1qdlx4ro1_1280.jpg");
 
+    $('.tlt').textillate({
+        in: { effect: 'tada'},
+        out: { effect: 'pulse', sync: true },
+        loop: true
+
+        });
+
 });
